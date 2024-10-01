6fec3544e596a2e69975e1d8ebedde49e88040e4
Dorian Devua <Dorian.DevUa@gmail.com>
2024-10-01 17:34:49 +0400

    finish js

--- js/task-3.js
+++ js/task-3.js
@@ -3,9 +3,10 @@ const refs = {
   outputName: document.querySelector('#name-output'),
 };
 
-const onChangeInputName = () => {
-  const normalizedInput = refs.inputName.value.trim();
-  const DEFAULT_NAME = 'Anonymous';
+const DEFAULT_NAME = 'Anonymous';
+
+const onChangeInputName = evt => {
+  const normalizedInput = evt.currentTarget.value.trim();
 
   refs.outputName.textContent = normalizedInput || DEFAULT_NAME;
 };
